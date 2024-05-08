059b033b81c0c0c3676a18ec7ee470462355b7af
Faith (Naza) Obasi <122112316+Naza6@users.noreply.github.com>
2024-05-08 00:31:25 -0400

    Update Back-up Script.ps1

--- Back-up Script.ps1
+++ Back-up Script.ps1
@@ -1,36 +1,26 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww15160\viewh16800\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
 \f0\fs24 \cf0 \
 # Prompt for user name\
 $name = Read-Host "Hello! What's your name?"\
 \
-# Source directory to be backed up\
+# Source dir to be backed up\
 $sourceDir = "C:\\Users\\****\\Desktop\\Videos"\
 \
-# Destination directory for backups\
+# Destination dir for backups\
 $backupDir = "C:\\Shared\\Home"\
 \
-# Create a timestamp for the backup file\
+# Timestamp for the backup file\
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"\
 \
-# Create the backup file name\
+# Creating the backup file name\
 $backupFile = "$backupDir\\backup_$timestamp.tar.gz"\
 \
-# Perform the backup\
+# Performing the backup\
 Compress-Archive -Path $sourceDir -DestinationPath $backupFile\
 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \expnd0\expndtw0\kerning0
 # Email configuration\
-$smtpServer = "your_smtp_server"\
-$from = "your_email@example.com"\
-$to = "recipient@example.com"\
+$smtpServer = "####smtp_server"\
+$from = "#####@#####.com"\
+$to = "####@#####.com"\
 $subject = "Backup Completed"\
 $body = "Hello, $name! Your backup is completed: $backupFile"\
 \'a0\

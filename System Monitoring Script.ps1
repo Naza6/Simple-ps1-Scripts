f519ed6c430e2639f726cc3eaeee7b2a7d30c5a1
Faith (Naza) Obasi <122112316+Naza6@users.noreply.github.com>
2024-05-08 00:34:28 -0400

    Update System Monitoring Script.ps1

--- System Monitoring Script.ps1
+++ System Monitoring Script.ps1
@@ -1,11 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 # Prompt for user name\
+# Prompt for user name\
 $name = Read-Host "Hello! What's your name?"\
 \
 # Monitor CPU usage\
@@ -17,13 +10,13 @@ $memFree = (Get-WmiObject -Class Win32_OperatingSystem).FreePhysicalMemory\
 $memUsagePercent = ($memUsage - $memFree) / $memUsage * 100\
 \
 # Monitor disk usage\
-$diskUsage = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'" | Select-Object -ExpandProperty FreeSpace\
-$diskUsagePercent = ($diskUsage / (Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'" | Select-Object -ExpandProperty Size)) * 100\
+$diskUsage = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:###'" | Select-Object -ExpandProperty FreeSpace\
+$diskUsagePercent = ($diskUsage / (Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:###'" | Select-Object -ExpandProperty Size)) * 100\
 \
 # Email configuration\
-$smtpServer = \'93****_server"\
-$from = \'93****@****.com"\
-$to = \'93****@****.com"\
+$smtpServer = \'####smpt_server"\
+$from = \'####@####.com"\
+$to = \'####@####.com"\
 $subject = "System Monitoring Report"\
 $body = @"\
 Hello, $name!\
